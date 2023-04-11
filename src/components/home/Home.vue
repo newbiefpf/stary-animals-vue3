f989f7c73b39ac5ea4842cd807d0aeef2ae6a838
newbiefpf <1326788346@qq.com>
2023-04-11 23:32:05 +0800

    home接口

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,13 +6,13 @@
         首页
       </BreadcrumbItem>
     </Breadcrumb>
-    <el-divider/>
-    <Row style="max-height: 330px;height: 400px;overflow: hidden">
+    <el-divider />
+    <Row style="max-height: 330px; height: 400px; overflow: hidden">
       <Col span="8" class="ivu-pr-16">
         <el-carousel>
           <div class="smallpic_item">
             <el-carousel-item v-for="(item, index) in urls" :key="index">
-              <img :src="item.url" style="width:100%; height:100%">
+              <img :src="item.url" style="width: 100%; height: 100%" />
               <h5>保护动物，你我共同的使命</h5>
             </el-carousel-item>
           </div>
@@ -22,7 +22,11 @@
         <Title :level="4">热点资讯</Title>
         <Paragraph>
           <ul>
-            <li v-for="item in news" style="align-items: center;user-select: none;" @click="push('/realtime_view?id='+item.id)">
+            <li
+              v-for="item in news"
+              style="align-items: center; user-select: none"
+              @click="push('/realtime_view?id=' + item.id)"
+            >
               <el-text tag="b" class="w-350px" truncated>
                 {{ item.title }}
               </el-text>
@@ -36,7 +40,10 @@
         <Paragraph>
           <div>
             <ul>
-              <li v-for="item in news" @click="push('/realtime_view?id='+item.id)">
+              <li
+                v-for="item in news1"
+                @click="push('/realtime_view?id=' + item.id)"
+              >
                 <el-text tag="b" truncated>{{ item.title }}</el-text>
               </li>
             </ul>
@@ -45,28 +52,42 @@
       </Col>
     </Row>
     <Title :level="3" class="ivu-mt-16 ivu-mb-16">最新领养</Title>
-    <div style="display: flex;flex-wrap:wrap">
-      <div v-for="item in newAdopts" style="width: 255px;user-select: none" @click="push('/adopt_view?id='+item.id)">
-        <div style="background:#f8f8f8;padding: 3px">
+    <div style="display: flex; flex-wrap: wrap">
+      <div
+        v-for="item in newAdopts"
+        style="width: 255px; user-select: none"
+        @click="push('/adopt_view?id=' + item.id)"
+      >
+        <div style="background: #f8f8f8; padding: 3px">
           <Card :bordered="false">
             <template #title>
-              <el-text size="small" style="width: 150px" truncated>{{ item.title }}</el-text>
-              <el-text size="small" class="ivu-fr">{{ item.time }}</el-text>
+              <el-text size="small" style="width: 150px" truncated>{{
+                item.title
+              }}</el-text>
+              <el-text size="small" class="ivu-fr">{{
+                item.gmtCreate
+              }}</el-text>
             </template>
-            <img :src="item.url" style="width:100%; height:100%">
+            <img :src="item.url" style="width: 100%; height: 100%" />
           </Card>
         </div>
       </div>
     </div>
     <Title :level="3" class="ivu-mt-16 ivu-mb-16">最新寻宠</Title>
     <div class="cardContainer">
-      <div v-for="item in newSeeks" style="width: 255px;background:#f8f8f8;padding: 3px" @click="push('/seek_view?id='+item.id)">
+      <div
+        v-for="item in newSeeks"
+        style="width: 255px; background: #f8f8f8; padding: 3px"
+        @click="push('/seek_view?id=' + item.id)"
+      >
         <Card :bordered="false">
           <template #title>
-            <el-text size="small" style="width: 150px" truncated>{{ item.title }}</el-text>
+            <el-text size="small" style="width: 150px" truncated>{{
+              item.title
+            }}</el-text>
             <el-text size="small" class="ivu-fr">{{ item.time }}</el-text>
           </template>
-          <img :src="item.url" style="width:100%; height:100%">
+          <img :src="item.url" style="width: 100%; height: 100%" />
         </Card>
       </div>
     </div>
@@ -74,153 +95,159 @@
 </template>
 
 <script>
-
 import router from "@/router";
 
+import { hotNewsList } from "@/http/api/baseDataApi";
+
 export default {
-  name: 'HomeComponent',
+  name: "HomeComponent",
   components: {},
   data() {
     // 白板 https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304
     return {
       urls: [
-        {url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304'},
-        {url: 'https://img0.baidu.com/it/u=1329903597,3197318107&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667'},
-        {url: 'https://img0.baidu.com/it/u=1329903597,3197318107&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667'}
+        {
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+        },
+        {
+          url: "https://img0.baidu.com/it/u=1329903597,3197318107&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667",
+        },
+        {
+          url: "https://img0.baidu.com/it/u=1329903597,3197318107&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=667",
+        },
       ],
-      news: [
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"},
-        {title: "这是信息这是信息这是信息这是信息这是信息这是信息"}
-      ],
+      news: [],
+      news1: [],
       newAdopts: [
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
-        }
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
+        },
       ],
       newSeeks: [
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
         },
         {
-          url: 'https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304',
-          title: '这是标题信息这是标题信息这是标题信息这是标题信息',
-          time: '3月18日22:02'
-        }
+          url: "https://img1.baidu.com/it/u=1410611840,1889651514&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=304",
+          title: "这是标题信息这是标题信息这是标题信息这是标题信息",
+          time: "3月18日22:02",
+        },
       ],
-    }
+    };
+  },
+  created() {
+    hotNewsList(0).then((res) => {
+      console.log(res);
+      this.news = res.content;
+    });
+    hotNewsList(1).then((res) => {
+      console.log(res);
+      this.news1 = res.content;
+    });
   },
   methods: {
     push(model) {
-      router.push(model)
-    }
-  }
-}
+      router.push(model);
+    },
+  },
+};
 </script>
-
 
 <style scoped>
 .smallpic_item h5 {
   position: absolute;
   left: 3rem;
   bottom: 1.2rem;
-  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-family: Georgia, "Times New Roman", Times, serif;
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
@@ -228,8 +255,8 @@ export default {
   display: flex;
   align-items: center;
   letter-spacing: 0.02em;
-  font-feature-settings: 'liga' off;
-  color: #FFFFFF;
+  font-feature-settings: "liga" off;
+  color: #ffffff;
 }
 
 .w-350px {
--- src/mock/index.js
+++ src/mock/index.js
@@ -1,9 +1,9 @@
-import Mock from 'mockjs'
-import './adopt'
-import './seek'
-import './rescue'
-import './baseData'
+// import Mock from 'mockjs'
+// import './adopt'
+// import './seek'
+// import './rescue'
+// import './baseData'
 
-Mock.setup({timeout: 300})
+// Mock.setup({timeout: 300})
 
-export default Mock
+// export default Mock
